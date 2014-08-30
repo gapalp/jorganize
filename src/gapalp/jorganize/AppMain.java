b6f27cc0427640c29bd87687daa75b38b2529cc3
gapalp <gparamore@gmail.com>
2014-08-30 15:14:25 -0400

    changed

--- src/gapalp/jorganize/AppMain.java
+++ src/gapalp/jorganize/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		
 		
-        //System.out.println("Hello World!"); // Display the string.
+        System.out.println("Hello World!"); // Display the string.
     }
 
 }
